525f9e9285bc14c7beeaa5978c448137408b27e9
Abhishek <abhishekraj0352@gmail.com>
2022-12-20 21:25:31 +0530

    Put_request

---
+++ src/main/java/restassuredTests/PUT_Request.java
@@ -0,0 +1,37 @@
+package restassuredTests;
+
+import java.util.HashMap;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+
+public class PUT_Request {
+
+	public static HashMap map =new HashMap();
+	int id= 21;
+	
+	@BeforeClass
+	public void postdata() {
+		map.put("employee_name",Utils.employee_name());
+		map.put("employee_salary",Utils.employee_salary());
+		map.put("employee_age",Utils.employee_age());
+		
+		RestAssured.baseURI ="https://dummy.restapiexample.com/api/v1/";
+		RestAssured.basePath="/update/"+id;
+	}
+	
+	@Test
+	public void putTest(){
+		
+		given().contentType("application/json").body(map)
+		
+		.when().put()
+		
+		.then().statusCode(200).log().all();
+	
+	}
+	
+}
--- src/main/java/restassuredTests/Utils.java
+++ src/main/java/restassuredTests/Utils.java
@@ -9,19 +9,29 @@ public class Utils {
 		return ("John"+generatedString);
 	}
 	
+	public static String employee_name() {
+		String generatedString = RandomStringUtils.randomAlphabetic(1);
+		return ("John"+generatedString);
+	}
+
 	public static String getJob() {
 		String generatedString = RandomStringUtils.randomAlphabetic(1);
 		return ("kumar"+generatedString);
 	}
 	
-	public static String getUserName() {
+	public static String getUserName(){
 		String generatedString = RandomStringUtils.randomAlphabetic(1);
 		return ("kumar"+generatedString);
 	}
 	
 	
-	public static String getPassword() {
-		String generatedString = RandomStringUtils.randomAlphabetic(1);
-		return ("kumar"+generatedString);
+	public static String employee_salary(){
+		String generatedString = RandomStringUtils.randomNumeric(5);
+		return generatedString;
+	}
+	
+	public static String employee_age(){
+		String generatedString = RandomStringUtils.randomNumeric(2);
+		return generatedString;
 	}
 }
